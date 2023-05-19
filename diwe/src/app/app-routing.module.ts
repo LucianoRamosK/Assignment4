eb586e66e515df6788b65f3bc5525478bf45a3b8
LucianoRamosK <69920264+LucianoRamosK@users.noreply.github.com>
2023-05-19 21:22:49 +0100

    3 files modified, 2 added

--- diwe/src/app/app-routing.module.ts
+++ diwe/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect the root URL to the home route
+  { path: 'home', component: HomeComponent }, // Add a route for the HomeComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- diwe/src/app/home/home.component.ts
+++ diwe/src/app/home/home.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  createPost() {}
+  posts: any[] = []; //posts
+
+  newPostTitle: string = ''; //Title
+  newPostUrl: string = ''; //Url
+  newComment: string = ''; //Comment
+
+  createPost() {
+    const newPost = {
+      title: this.newPostTitle,
+      url: this.newPostUrl,
+      votes: 0,
+      comments: [],
+    };
+    this.posts.push(newPost);
+
+    this.newPostTitle = ''; // Clear the input field after creating the post
+    this.newPostUrl = ''; // Clear the input field after creating the post
+  }
+
+  upvotePost(post: any) {
+    post.votes++;
+  }
+
+  showComments(post: any) {
+    this.showCommentsForPost = post;
+  }
+
+  addComment(post: any) {
+    const newComment = this.newComment;
+    post.comments.push(newComment);
+    this.newComment = ''; // Clear the input field after adding the comment
+  }
+  showCommentsForPost: any; // Added showCommentsForPost property
 }
