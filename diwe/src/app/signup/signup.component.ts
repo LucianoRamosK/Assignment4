3565d42e20ecf21f205427e557a13d8602097604
LucianoRamosK <69920264+LucianoRamosK@users.noreply.github.com>
2023-05-21 10:43:08 +0100

    2 files modified

--- diwe/src/app/signup/signup.component.ts
+++ diwe/src/app/signup/signup.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  email: string = ''; // Initialize email with an empty string
+  password: string = ''; // Initialize password with an empty string
 
+  constructor(private http: HttpClient) {}
+
+  signUp(): void {
+    const signUpData = { email: this.email, password: this.password };
+    this.http.post('/api/signup', signUpData).subscribe(
+      (response) => {},
+      (error) => {}
+    );
+  }
 }
