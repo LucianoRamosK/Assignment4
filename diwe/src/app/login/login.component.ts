786f3d1a8f2d464d219e5d4eb050d5eed54814b3
LucianoRamosK <69920264+LucianoRamosK@users.noreply.github.com>
2023-05-21 11:29:38 +0100

    2 files modified

--- diwe/src/app/login/login.component.ts
+++ diwe/src/app/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  login(): void {
+    const loginData = { email: this.email, password: this.password };
+    this.http.post('/api/login', loginData).subscribe(
+      (response) => {
+        // Handle login
+      },
+      (error) => {
+        // Handle error
+      }
+    );
+  }
 }
